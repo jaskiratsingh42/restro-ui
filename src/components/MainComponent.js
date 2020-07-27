99e0dff4147c2a3c564dcd68374b5104fa044760
Jaskirat Singh <jaskiratsingh4242@gmail.com>
2020-07-27 14:11:10 +0530

    Added Header & Footer Components

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Navbar, NavbarBrand } from "reactstrap";
+import Header from "./HeaderComponent";
+import Footer from "./FooterComponent";
 import Menu from "./MenuComponent";
 import DishDetail from "./DishDetailComponent";
 import { DISHES } from "../shared/dishes";
@@ -20,11 +22,7 @@ class Main extends Component {
   render() {
     return (
       <div>
-        <Navbar dark color="primary">
-          <div className="container">
-            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
-          </div>
-        </Navbar>
+        <Header />
         <div className="container">
           <Menu
             dishes={this.state.dishes}
@@ -38,6 +36,7 @@ class Main extends Component {
             }
           />
         </div>
+        <Footer />
       </div>
     );
   }
