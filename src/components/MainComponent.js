5e9461b47b0303cd024669a5a356c5944d5f6429
Jaskirat Singh <jaskiratsingh4242@gmail.com>
2020-07-28 11:35:57 +0530

    Added Contact Component

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,6 +4,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Menu from "./MenuComponent";
+import Contact from "./ContactComponent";
 import DishDetail from "./DishDetailComponent";
 import { DISHES } from "../shared/dishes";
 
@@ -30,6 +31,7 @@ class Main extends Component {
               path="/menu"
               component={() => <Menu dishes={this.state.dishes} />}
             />
+            <Route exact path="/contact" component={Contact} />
             <Redirect to="/home" />
           </Switch>
         </div>
