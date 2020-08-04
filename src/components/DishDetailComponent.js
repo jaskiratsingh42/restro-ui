9360f7d43234fad6fb248969876628b4699f8d43
Jaskirat Singh <jaskiratsingh4242@gmail.com>
2020-08-04 22:26:06 +0530

    Added Redux Thunk & Dishes Loading Actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -18,6 +18,7 @@ import {
 } from "reactstrap";
 import { Link } from "react-router-dom";
 import { Control, LocalForm, Errors } from "react-redux-form";
+import { Loading } from "./LoadingComponent";
 
 // Comment Form Class Component Starts here
 class CommentForm extends Component {
@@ -197,8 +198,23 @@ function RenderComments({ comments, addComment, dishId }) {
   }
 }
 const DishDetail = (props) => {
-  if (props.dish == null) return <div></div>;
-  else {
+  if (props.isLoading) {
+    return (
+      <div className="container">
+        <div className="row">
+          <Loading />
+        </div>
+      </div>
+    );
+  } else if (props.errMess) {
+    return (
+      <div className="container">
+        <div className="row">
+          <h4>{props.errMess}</h4>
+        </div>
+      </div>
+    );
+  } else if (props.dish != null) {
     return (
       <div className="container">
         <div className="row">
